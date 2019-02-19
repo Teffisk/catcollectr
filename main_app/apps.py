867b22af0e6ab575778f4c00926da094b932ce66
Teffisk <brad.mcknight91@gmail.com>
2019-02-19 11:54:52 -0800

    completed all steps of the hw, still have questions

---
+++ main_app/apps.py
@@ -0,0 +1,18 @@
+from django.apps import AppConfig
+
+
+class MainConfig(AppConfig):
+    name = 'main_app'
+
+$('.btn').on('click', function(event){
+  event.preventDefault();
+  var element = $(this);
+  $.ajax({
+    url: '/like_cat/',
+    method: 'GET',
+    data: {cat_id: element.attr('data-id')},
+    success: function(response){
+      element.html('Likes: ' + response);
+    }
+  })
+})
